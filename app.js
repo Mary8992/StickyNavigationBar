549e37720967c6b7a5cf88585da49bacabc98cc9
Mary8992 <mc046569@gmail.com>
2023-01-14 14:29:55 +0300

    CSS3 Transform Effects on Scroll added

--- app.js
+++ app.js
@@ -2,3 +2,11 @@ window.addEventListener("scroll", function () {
     let header = document.querySelector("header");
     header.classList.toggle("sticky", window.scrollY > 0);
 });
+
+//CSS3 Transform Effects on Scroll
+let scroll = document.querySelector('.curve');
+window.addEventListener('scroll', function() {
+    //if the vh is 200 it is better to devide scrollY by -500
+    let value = 1 + window.scrollY / -800;
+    scroll.style.transform = `scaleY(${value})`;
+})
